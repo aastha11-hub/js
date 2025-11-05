d6b51a37a7755412f9bcb1ab5c0008061a4cda23
Aastha <aasthaisrani1106@gmail.com>
2025-11-05 16:33:58 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "aastha1106@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad" 
+let accountState;
+
+//accountId = 2
+accountEmail = "h@gmail.com"
+accountPassword = "6789"
+accountCity = "Delhi"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+let name = "Aastha"
+let age = 18
+let isApproved = false
+
+//number
+//bigint
+//boolean
+//string
+//null => stand alone value
+//undefined => not assigned(not written anything) 
+//symbol => unique (figma tools)
+
+//object
+console.log(typeof "Aastha"); 
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
