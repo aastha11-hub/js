df979860da327417c34226eb481e4295555fe27c
Aastha <aasthaisrani1106@gmail.com>
2025-11-07 15:14:36 +0000

    container config

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log(2 > 1);
+//console.log(2 < 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+ //always compare same data types
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+//for undefined its false for all
+
+// === 
+console.log("2" === 2); //false
+//because data type are different
