b0e13a5c12f1fdea07436f56ba5d6a7f3a004be9
Aastha <aasthaisrani1106@gmail.com>
2025-11-19 15:29:49 +0000

    container config

---
+++ 02_basics/02_array.js
@@ -0,0 +1,23 @@
+const marvel_heros =["thor", "ironman", "spiderman"]
+const dc_heros =["superman", "batman", "flash"]
+marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros);
+
+marvel_heros.concat(dc_heros)
+//console.log(marvel_heros);
+//push and concat are same combines two arrays
+
+//const all_heros = marvel_heros.concat(dc_heros)
+//console.log(all_heros);
+//merging both arrays
+
+const all_heros = [...marvel_heros, ...dc_heros]
+console.log(all_heros);
+//using spread operator to merge two arrays
+
+const another_array = [1,2,3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+//flattening an array using flat method
+
--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -15,5 +15,17 @@ console.log(myArr[0]);
 //console.log(myArr.includes(9));
 //console.log(myArr.indexOf(3));
 
-const newArr = myArr.join
-console.log(myArr);
+//const newArr = myArr.join
+//console.log(myArr);
+//console.log( newArr);
+
+//slice, splice
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("8 ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
